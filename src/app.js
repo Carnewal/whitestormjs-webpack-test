e04cb2607b7aaf991525e0b93aadff61b9f4a14b
Carnewal <bradster@live.be>
2016-08-15 21:00:02 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import * as WHS from 'whitestormjs'
+
+const world = new WHS.World({
+    stats: "fps", // fps, ms, mb or false if not need.
+    autoresize: true,
+
+    gravity: { // Physic gravity.
+        x: 0,
+        y: -100,
+        z: 0
+    }
+});
+
+const sphere = new WHS.Sphere({ // Create sphere object.
+  geometry: {
+    radius: 3
+  },
+
+  mass: 10, // Mass of physics object.
+
+  material: {
+    color: 0xffffff,
+    kind: 'basic'
+  },
+
+  pos: {
+    x: 0,
+    y: 100,
+    z: 0
+  }
+});
+
+sphere.addTo(GAME);
+sphere.getNative(); // Returns THREE.Mesh of this object.
+
+world.start(); // Start animations and physics simulation.
